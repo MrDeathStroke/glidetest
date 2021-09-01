befbf3f6882eb1e782bd3a86b88ed5226fa63c19
Purvang Joshi <purvang.joshi4@gmail.com>
2021-09-01 16:57:53 +0530

    Update function.js

--- function.js
+++ function.js
@@ -1,8 +1,8 @@
 window.function = function(Email) 
 {
 if (Email.value === undefined) return undefined;
-  const response = await fetch('http://apilayer.net/api/validate?access_key=9c511ff979f422c3c1bb7cdd2dbe080f&number=${Email.value}');
-  const data = await response.json();
-  return ${data.country_code};
+  const response = await fetch(`http://apilayer.net/api/validate?access_key=9c511ff979f422c3c1bb7cdd2dbe080f&number=${Email.value}`);
+  const {data} = await response.json();
+  return data.country_code;
   
 }
